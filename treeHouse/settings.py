42eb5b46b0e9f715085b65dc8eb46bc1f7ef04b6
cbrianbet <cbrianbet@outlook.com>
2026-04-08 23:39:31 +0300

    Update settings.py

--- treeHouse/settings.py
+++ treeHouse/settings.py
@@ -157,9 +157,9 @@ REST_AUTH = {
     # 'PASSWORD_RESET_SERIALIZER': 'authentication.serializers.CustomPasswordResetSerializer'
 }
 
-CORS_ALLOWED_ORIGINS = [
-    'http://localhost:3000',
-    'http://127.0.0.1:3000',
+CORS_ALLOWED_ORIGIN_REGEXES = [
+    r'^http://localhost:\d+$',
+    r'^http://127\.0\.0\.1:\d+$',
 ]
 CORS_ALLOW_CREDENTIALS = True
 
